a79d4142e465aa191d12f704ec2ad30697b3ba73
Skynetjosh98 <101459268+Skynetjosh98@users.noreply.github.com>
2022-04-10 11:54:59 -0700

    my 1st commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main - assigns a random number to int and eachtime
+ * main - assigns a random number to int n everytime
  * it executes, and prints it
- * Return: always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
