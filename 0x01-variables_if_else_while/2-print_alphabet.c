127fd8205e63dab6e4704bd82269d26e097b5ddf
Skynetjosh98 <joshuz1998@gmail.com>
2022-04-10 22:35:05 -0700

    my 3rd commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - script that prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp;
+
+	for (alp = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
