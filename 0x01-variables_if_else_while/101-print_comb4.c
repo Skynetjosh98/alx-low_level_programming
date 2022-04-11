f64a4f11f42ffb3fa043fc5c151dc4c01e6e4efe
Skynetjosh98 <joshuz1998@gmail.com>
2022-04-11 00:01:12 -0700

    my 12th commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible different combinations of three digits
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1, num2, l;
+
+	for (num1 = 48; num1 < 58; num1++)
+	{
+		for (num2 = 49; num2 < 58; num2++)
+		{
+			for (l = 50; l < 58; l++)
+			{
+				if (l > num2 && nu2m > num1)
+				{
+					putchar(num1);
+					putchar(num2);
+					putchar(l);
+					if (num1 != 55 || num2 != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
