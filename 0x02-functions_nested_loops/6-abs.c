91a02f170b78af816a142d6572691113f0846305
Skynetjosh98 <joshuz1998@gmail.com>
2022-04-12 18:39:19 -0700

    my 7th commit

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -5,11 +5,11 @@
  * @n: the int to check
  * Return: the absolute value of int
  */
-int _abs(int ab)
+int _abs(int n)
 {
-	if (ab >= 0)
+	if (n >= 0)
 	{
-		return (ab);
+		return (n);
 	}
-	return (-ab);
+	return (-n);
 }
